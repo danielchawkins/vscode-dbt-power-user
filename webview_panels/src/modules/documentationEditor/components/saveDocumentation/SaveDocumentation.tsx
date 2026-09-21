c24f5838b82559f512230a2ba085afe1841e9c9a
Daniel Hawkins <dhawkins1234@gmail.com>
2026-09-20 21:34:14 -0700

    fix: drop antd noop from documentation editor

--- webview_panels/src/modules/documentationEditor/components/saveDocumentation/SaveDocumentation.tsx
+++ webview_panels/src/modules/documentationEditor/components/saveDocumentation/SaveDocumentation.tsx
@@ -10,12 +10,13 @@ import {
 import { MouseEvent, useEffect, useRef, useState } from "react";
 import { setIncomingDocsData } from "@modules/documentationEditor/state/documentationSlice";
 import classes from "../../styles.module.scss";
-import { noop } from "antd/es/_util/warning";
 import {
   DBTDocumentation,
   DBTModelTest,
 } from "@modules/documentationEditor/state/types";
 import { isStateDirty } from "@modules/documentationEditor/utils";
+
+const noop = (): void => undefined;
 
 /**
  * Handles save documentation functionality
