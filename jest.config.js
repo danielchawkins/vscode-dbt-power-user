0c0f1d624463538641d25556800237058cd034f9
Daniel Hawkins <dhawkins1234@gmail.com>
2026-09-20 18:33:01 -0700

    test: compile integration harness as CommonJS under root ESM

--- jest.config.js
+++ jest.config.js
@@ -22,6 +22,7 @@ export default {
   ],
   coverageDirectory: "coverage",
   moduleNameMapper: {
+    "^(\\.{1,2}/)+extensionRoot$": "<rootDir>/src/test/mock/extensionRoot.ts",
     "^vscode$": "<rootDir>/src/test/mock/vscode.ts",
     "^node-fetch$": "<rootDir>/src/test/mock/node-fetch.ts",
     "^@altimateai/dbt-integration$": "@altimateai/dbt-integration",
